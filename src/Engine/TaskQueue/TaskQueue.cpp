a34b79065c7cb053dc8a55bbdbb3692e44d34939
daydream <daydream@daydream.daydream>
2025-02-15 00:59:57 +0100

    Added TaskQueue

---
+++ src/Engine/TaskQueue/TaskQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daydream on 15.02.25.
+//
+
+#include "TaskQueue.h"
---
+++ src/Engine/TaskQueue/TaskQueue.h
@@ -0,0 +1,16 @@
+//
+// Created by daydream on 15.02.25.
+//
+
+#ifndef TASKQUEUE_H
+#define TASKQUEUE_H
+
+
+
+class TaskQueue {
+
+};
+
+
+
+#endif //TASKQUEUE_H
