65443729f9d09ee153e90ac7511e7341ca5e0979
daydream <daydream@daydream.daydream>
2025-02-15 04:07:51 +0100

    TaskQueue Base

--- src/Engine/TaskQueue/TaskQueue.cpp
+++ src/Engine/TaskQueue/TaskQueue.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "TaskQueue.h"
+
+
+bool FentEngine::TaskQueue::isEmpty() const {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    return m_queue.empty();
+}
+
+void FentEngine::TaskQueue::enqueueTask(const std::function<void()>& task) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_queue.push(task);
+    m_cv.notify_one();
+}
+
+void FentEngine::TaskQueue::executeNextTask() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_cv.wait(lock, [this]() { return !m_queue.empty(); });
+
+    Task task = m_queue.front();
+}
+
+void FentEngine::TaskQueue::executeAllTasks() {
+    while (!isEmpty()) {
+        executeNextTask();
+    }
+}
--- src/Engine/TaskQueue/TaskQueue.h
+++ src/Engine/TaskQueue/TaskQueue.h
@@ -4,12 +4,30 @@
 
 #ifndef TASKQUEUE_H
 #define TASKQUEUE_H
+#include <condition_variable>
+#include <functional>
+#include <queue>
 
 
+namespace FentEngine {
+    class TaskQueue {
+    private:
+        using Task = std::function<void()>;
 
-class TaskQueue {
+        std::queue<std::function<void()>> m_queue;
+        mutable std::mutex m_mutex;
+        std::condition_variable m_cv;
 
-};
+    public:
+        TaskQueue() = default;
+        ~TaskQueue() = default;
+
+        bool isEmpty() const;
+        void enqueueTask(const std::function<void()>& task);
+        void executeNextTask();
+        void executeAllTasks();
+    };
+}
 
 
 
